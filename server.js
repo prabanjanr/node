66ecb1cecdfa0e6905cf430cd98bcfc7cbbdca1a
prabanjanr <prabkarnan18@gmail.com>
2023-04-07 22:00:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts",(req,res)=>{
+    res.json({message: "get all"});
+})
+
+app.listen(port, () => {
+  console.log("Server running on port ${port}");
+});
